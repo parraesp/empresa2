802901af036462d23a53d0d68b11f2f5595b6949
parraesp <alc.htc.user@gmail.com>
2014-12-05 12:17:08 +0100

    reestructuracion mejorada

--- tests/test_departamento.py
+++ tests/test_departamento.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
-from Departamento import *
 from mockito import *
-from Empleado import *
 __author__ = 'Alberto L'
-
+from src.Empleado import *
+from src.Departamento import *
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 class TestDepartamento(TestCase):
     """
